3e508785d47f356ab72523de3dff5d4751aff738
rvanco <98898021+rvanco@users.noreply.github.com>
2022-07-19 00:49:45 +0200

    rules are implemented
    a "border" as been add to the lattice because I didn't find how to create a matrix with periodical boundaries e.g. : [i-1] on the top line wasn't possible
    rules are implemented

--- game_of_life.jl
+++ game_of_life.jl
@@ -1,5 +1,7 @@
 # Create the grid :
-matrix_base = zeros(10, 10)
+lines = 10
+columns = 10
+matrix_base = zeros(lines, columns)
 ij_randoms = Array{Int}(undef, 0, 2)
 i = 0
 while i < 20
@@ -31,9 +33,64 @@ plot(
     Layout(xaxis_showgrid=true, yaxis_showgrid=true)
 )
 
+left_border = zeros(lines, 1)
+top_border = zeros(1, columns+1)
+right_border = zeros(lines+1, 1)
+bottom_border = zeros(1, columns+2)
 
+matrix_base = hcat(left_border, matrix_base)
+matrix_base = vcat(top_border, matrix_base)
+matrix_base = hcat(matrix_base, right_border)
+matrix_base = vcat(matrix_base, bottom_border)
 
-# create the rules :
+data2 = matrix_base
+plot(
+    heatmap(
+        z=data2,
+        colorscale="Greys",
+    ),
+    Layout(xaxis_showgrid=true, yaxis_showgrid=true)
+)
+
+matrix_base[4,:]
+
+# FOR LOOP
+new_matrix = zeros(lines+2, columns+2)
+for i in 2:lines
+    for j in 2:columns
+        neighbors = matrix_base[i-1, j-1] + matrix_base[i-1, j] + matrix_base[i-1, j+1] + matrix_base[i, j-1] + matrix_base[i, j+1] + matrix_base[i+1, j-1] + matrix_base[i+1, j] + matrix_base[i+1, j+1]
+
+        # applying the rules :
+
+        # If a cell is ON :
+        if matrix_base[i, j] == 1
+            # and has fewer than two neighbors that are ON, it turns OFF
+            if neighbors < 2
+                new_matrix[i, j] = 0
+            # and has more than three neighbors that are ON, it turns OFF
+            elseif neighbors > 3
+                new_matrix[i, j] = 0
+            # and has either two or three neighbors that are ON, it remains ON
+            else
+                new_matrix[i, j] = 1
+            end
+        end
+
+        # If a cell is OFF and has exactly three neighbors that are ON, it turns ON.
+        if matrix_base[i, j] == 0 && neighbors == 3
+            new_matrix[i, j] = 1
+        end
+    end
+end
+
+data3 = new_matrix
+plot(
+    heatmap(
+        z=data3,
+        colorscale="Greys",
+    ),
+    Layout(xaxis_showgrid=true, yaxis_showgrid=true)
+)
 
 
 
