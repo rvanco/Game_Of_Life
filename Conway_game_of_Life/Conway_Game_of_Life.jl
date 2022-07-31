6f962a5d4ee9ff1909ca24d4b3fbb44f799b9820
rvanco <98898021+rvanco@users.noreply.github.com>
2022-07-31 23:05:33 +0200

    Folder

---
+++ Conway_game_of_Life/Conway_Game_of_Life.jl
@@ -0,0 +1,73 @@
+using Plots
+
+# Create the grid :
+lines = 100
+columns = 100
+matrix_base = zeros(lines, columns)
+ij_randoms = Array{Int}(undef, 0, 2)
+i = 0
+while i < (lines*columns) *  10/100
+    ij_random = rand(1:lines, (1,2))   # MUST BE A SQUARE => lines=columns
+    ij_randoms = vcat(ij_randoms, ij_random)
+    i += 1
+end
+
+
+for i in 1:length(ij_randoms[:,1])
+    matrix_base[ij_randoms[i,1], ij_randoms[i,2]] = 1
+end
+
+
+# Add some border to be able to check every where :
+left_border = zeros(lines, 1)
+top_border = zeros(1, columns+1)
+right_border = zeros(lines+1, 1)
+bottom_border = zeros(1, columns+2)
+
+matrix_base = hcat(left_border, matrix_base)
+matrix_base = vcat(top_border, matrix_base)
+matrix_base = hcat(matrix_base, right_border)
+matrix_base = vcat(matrix_base, bottom_border)
+
+
+show_matrix = heatmap(matrix_base)
+
+path = ""
+savefig(show_matrix, path)
+
+anim = @animate for i in 1:100
+    # Update the grid with all the rules :
+    new_matrix = zeros(lines+2, columns+2)
+    global matrix_base
+    for i in 2:lines
+        for j in 2:columns
+            neighbors = matrix_base[i-1, j-1] + matrix_base[i-1, j] + matrix_base[i-1, j+1] + matrix_base[i, j-1] + matrix_base[i, j+1] + matrix_base[i+1, j-1] + matrix_base[i+1, j] + matrix_base[i+1, j+1]
+
+            # applying the rules :
+
+            # If a cell is ON :
+            if matrix_base[i, j] == 1
+                # and has fewer than two neighbors that are ON, it turns OFF
+                if neighbors < 2
+                    new_matrix[i, j] = 0
+                # and has more than three neighbors that are ON, it turns OFF
+                elseif neighbors > 3
+                    new_matrix[i, j] = 0
+                # and has either two or three neighbors that are ON, it remains ON
+                else
+                    new_matrix[i, j] = 1
+                end
+            end
+
+            # If a cell is OFF and has exactly three neighbors that are ON, it turns ON.
+            if matrix_base[i, j] == 0 && neighbors == 3
+                new_matrix[i, j] = 1
+            end
+        end
+    end
+    matrix_base = copy(new_matrix)
+    heatmap(matrix_base)
+end
+
+path2 = ""
+gif(anim, path2, fps = 10)
