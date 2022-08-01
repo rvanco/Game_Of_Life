dc0110209864ad20c56e3dd839e95cfca634b8af
rvanco <98898021+rvanco@users.noreply.github.com>
2022-08-01 21:13:50 +0200

    Add files via upload

--- Personnal_modification/world_map.jl
+++ Personnal_modification/world_map.jl
@@ -1,85 +1,17 @@
-using Plots
+using Images, Colors
 
-# Create the grid :
-lines = 2070
-columns = 3300
-matrix_base = zeros(lines, columns)
+map_black_white = "/Users/renaudvanco/Desktop/Julia/Map_black_white.png"
+img_continent = load(map_black_white)
+img = Gray.(img_continent)
+mat = convert(Array{Float64}, img)
 
-
-
-
-for i in 1649:1670
-    for j in 750:980
-        matrix_base[i, j] = 1
+for i in 1:785
+    for j in 1:1303
+        mat[i,j] = round(Int, mat[i, j])
     end
 end
 
-for i in 1627:1649
-    for j in 710:890
-        matrix_base[i, j] = 1
-    end
-    for j in 970:1020
-        matrix_base[i, j] = 1
-    end
-    for j in 1720:1810
-        matrix_base[i, j] = 1
-    end
-end
+mask_continent = colorview(Gray, mat)
 
-for i in 1606:1627
-    for j in 760:850
-        matrix_base[i, j] = 1
-    end
-    for j in 1000:1280
-        matrix_base[i, j] = 1
-    end
-    for j in 1720:1765
-        matrix_base[i, j] = 1
-    end
-    for j in 2530:2560
-        matrix_base[i, j] = 1
-    end
-end
-
-for i in 1584:1606
-    for j in 510:530
-        matrix_base[i, j] = 1
-    end
-    for j in 740:820
-        matrix_base[i, j] = 1
-    end
-    for j in 980:1300
-        matrix_base[i, j] = 1
-    end
-    for j in 2100:2150
-        matrix_base[i, j] = 1
-    end
-    for j in 2340:2620
-        matrix_base[i, j] = 1
-    end
-end
-
-for i in 1563:1584
-    for j in 420:510
-        matrix_base[i, j] = 1
-    end
-    for j in 590:610
-        matrix_base[i, j] = 1
-    end
-    for j in 770:870
-        matrix_base[i, j] = 1
-    end
-    for j in 980:1330
-        matrix_base[i, j] = 1
-    end
-    for j in 2080:2120
-        matrix_base[i, j] = 1
-    end
-    for j in 2340:2640
-        matrix_base[i, j] = 1
-    end
-end
-
-
-show_matrix = heatmap(matrix_base)
+save("gray.png", mask_continent)
 
